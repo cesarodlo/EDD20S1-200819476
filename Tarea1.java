00c2cd19de945e0fe4487e5d347f6894eb4633f2
Cesar Rodríguez <cesarodlo@gmail.com>
2020-01-27 20:49:55 -0600

    commit on 'master'

--- Tarea1.java
+++ Tarea1.java
@@ -1,5 +1,30 @@
+import java.util.*;
 class Tarea1{
     public static void main (String[]args){
-        System.out.println("Probando 123");
+        Scanner in=new Scanner(System.in);
+                
+        System.out.println("Ingresar tamaño de la matriz ");
+        int t=in.nextInt();
+        int matriz[][]=new int[t][t];
+        if(t>0){
+            //llenado de linea superior e inferior de la matriz
+            for(int i=0;i<t;i++){
+                matriz[0][i]=1;
+                matriz[t-1][i]=1;
+            }
+            //llenado de laterales de la matriz
+            for(int k=0;k<t-1;k++){
+                matriz[k][0]=1;
+                matriz[k][t-1]=1;
+            }
+        }
+        for(int i=0;i<t;i++){
+            for(int j=0;j<t;j++){
+                System.out.print(matriz[i][j]);
+                
+            }
+            System.out.println(" ");
+        }
+            
     }
 }
